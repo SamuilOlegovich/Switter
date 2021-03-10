4e8bfbdb304891f8a29b32b088ba6325e08c5d1a
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2021-03-10 15:03:36 +0200

    Spring Boot MVC: загрузка файлов на сервер и раздача статик (в частности в данном примере css)

--- src/main/java/swetter/controller/MainController.java
+++ src/main/java/swetter/controller/MainController.java
@@ -1,26 +1,40 @@
 package swetter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import swetter.model.db.Message;
 import swetter.model.db.User;
 import swetter.model.repo.MessageRepo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+    // ищет и вставляет из проперти
+    // --> upload.path=/Users/samuilolegovich/Documents/JAVA/Download
+    @Value("${upload.path}")
+    private String uploadPath;
+
+
+
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
         return "greet";
     }
+
+
 
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
@@ -37,17 +51,39 @@ public class MainController {
         return "main";
     }
 
+
+
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag,  Map<String, Object> model) {
+            @RequestParam String tag,  Map<String, Object> model,
+            // для загрузки файла
+            @RequestParam("file") MultipartFile file) throws IOException {
+
         Message message = new Message(text,tag, user);
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            // проверяем есть ли директория, если нет то создаем ее
+            if (!uploadDir.exists()) { uploadDir.mkdir(); }
+            // создаем уникальное имя файла
+            String uuidFile = UUID.randomUUID().toString();
+            // и к имени добавляем оригинальное название файла
+            // которое изначально загрузил пользователь
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+            // загружаем сам файл
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+            message.setFilename(resultFileName);
+        }
+
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
+
+
 
     // работал для проверки под mustache
 //    @PostMapping("filter")
--- src/main/java/swetter/securingweb/MvcConfig.java
+++ src/main/java/swetter/securingweb/MvcConfig.java
@@ -1,12 +1,22 @@
 package swetter.securingweb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    // ищет и вставляет из проперти
+    // --> upload.path=/Users/samuilolegovich/Documents/JAVA/Download
+    @Value("${upload.path}")
+    private String uploadPath;
+
+
+
 
     public void addViewControllers(ViewControllerRegistry registry) {
 //        registry.addViewController("/home").setViewName("home");
@@ -15,4 +25,15 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
     }
 
+
+    // для раздачи файла
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file://" + uploadPath + "/");
+        // чтобы раздавать статический контент без авторизации
+        registry.addResourceHandler("/static/**")
+                // classpath --> ищет папку в дереве проекта
+                .addResourceLocations("classpath:/static/");
+    }
 }
