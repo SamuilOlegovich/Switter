918d5920c578cc65909274de651c614a41a97b1f
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2021-03-22 15:58:59 +0200

    постраничное отображение длинных списков (pagination) - тем самым ускорениее отображения страницы

--- src/main/java/switter/controller/MainController.java
+++ src/main/java/switter/controller/MainController.java
@@ -1,6 +1,10 @@
 package switter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -38,16 +42,21 @@ public class MainController {
 
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
-        Iterable<Message> messages = messageRepo.findAll();
+    public String main(@RequestParam(required = false, defaultValue = "") String filter,
+                       Model model,
+                       // (пагинация)
+                       // делаем выборку и сортировку сообщений (самые первые будут показаны которые самые новые)
+                       @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Message> page;
 
         if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
+            page = messageRepo.findByTag(filter, pageable);
         } else {
-            messages = messageRepo.findAll();
+            page = messageRepo.findAll(pageable);
         }
 
-        model.addAttribute("messages", messages);
+        model.addAttribute("page", page);
+        model.addAttribute("url", "/main");
         model.addAttribute("filter", filter);
         return "main";
     }
--- src/main/java/switter/model/repo/MessageRepo.java
+++ src/main/java/switter/model/repo/MessageRepo.java
@@ -1,6 +1,8 @@
 package switter.model.repo;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import switter.model.db.Message;
 
@@ -9,5 +11,10 @@ import java.util.List;
 
 
 public interface MessageRepo extends JpaRepository<Message, Long> {
-    List<Message> findByTag(String tag);
+    // Pageable - для ускорения загрузки страницы когда много сообщений (пагинация)
+    // как итог будет возвращатся не List - a Page
+    Page<Message> findByTag(String tag, Pageable pageable);
+    // так же следует модицицировать findAll чтобы он принимал Page - аргумент
+    Page<Message> findAll(Pageable pageable);
+
 }
